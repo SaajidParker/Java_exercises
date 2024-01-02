cde6176e19bd5aae0bac088c329a59628adb3248
Saajid Parker <parkersaajid2@gmail.com>
2024-01-02 22:40:39 +0200

    String Challenge

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,8 +1,15 @@
 public class Exercises {
 
     public static void main(String[] args){
-
+        Exercises ex = new Exercises();
+        System.out.println(ex.stringTimes("Hello",5));
     }
-
+    public String stringTimes(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(str);
+        }
+        return result.toString();
+    }
 
 }
