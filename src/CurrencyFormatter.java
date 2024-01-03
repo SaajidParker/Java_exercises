c801acdbf2591d8784966f0dec9bd06b66f24075
Saajid Parker <parkersaajid2@gmail.com>
2024-01-03 23:04:23 +0200

    Made a currency formatter in java

---
+++ src/CurrencyFormatter.java
@@ -0,0 +1,17 @@
+import java.text.NumberFormat;
+import java.util.*;
+public class CurrencyFormatter {
+
+
+    public void Converter(double payment){
+
+        String  us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en ", "IN")).format(payment).replace("INR ", "Rs.");
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
--- src/Exercises.java
+++ src/Exercises.java
@@ -1,12 +1,17 @@
-import java.sql.SQLOutput;
-
+import java.util.Scanner;
 public class Exercises {
 
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
         StringChallengeOne call = new StringChallengeOne();
         System.out.println(call.stringTimes("hello",4));
         DayCalendarFinder find = new DayCalendarFinder();
         System.out.println(find.findDay(8,13,1999));
+        CurrencyFormatter formatting = new CurrencyFormatter();
+        double payment = scanner.nextDouble();
+        formatting.Converter(payment);
     }
 
 
