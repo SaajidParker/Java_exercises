ddcb9d5c3737919dbb1a6eef08cac6e38d3fb552
Saajid Parker <parkersaajid2@gmail.com>
2024-01-04 09:54:01 +0200

    Adding comments

--- src/Exercises.java
+++ src/Exercises.java
@@ -4,11 +4,13 @@ public class Exercises {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-
+        // String Challenge
         StringChallengeOne call = new StringChallengeOne();
         System.out.println(call.stringTimes("hello",4));
+        // Day finder in calendar
         DayCalendarFinder find = new DayCalendarFinder();
         System.out.println(find.findDay(8,13,1999));
+        //Currency Formatter
         CurrencyFormatter formatting = new CurrencyFormatter();
         double payment = scanner.nextDouble();
         formatting.Converter(payment);
