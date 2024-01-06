c044b49837283f821c3862ab796822169b941428
Saajid Parker <parkersaajid2@gmail.com>
2024-01-06 12:22:29 +0200

    Added a simple array exercise

--- src/Exercises.java
+++ src/Exercises.java
@@ -17,6 +17,9 @@ public class Exercises {
         //Username Validator using REGEX
         UsernameValidator valid = new UsernameValidator();
         valid.validate(1,"SaajidParker");
+        //Creating an array and calling the values added to it
+        FirstArrayExercise firstarray = new FirstArrayExercise();
+        firstarray.arr();
     }
 
 
