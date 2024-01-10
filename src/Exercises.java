f2b50d1f9bb347f52008cddb6c2d9508b174729b
Saajid Parker <parkersaajid2@gmail.com>
2024-01-10 12:09:15 +0200

    More comments

--- src/Exercises.java
+++ src/Exercises.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Exercises {
 
     public static void main(String[] args){
-
+        //Initializing scanner to be implemented in all the method classes
         Scanner scanner = new Scanner(System.in);
+
         // String Challenge
         StringChallengeOne call = new StringChallengeOne();
         System.out.println(call.stringTimes("HelloHowAreYou?",3));
+
         // Day finder in calendar
         DayCalendarFinder find = new DayCalendarFinder();
         System.out.println("Let's find out the day of the week you were born:");
@@ -19,30 +21,37 @@ public class Exercises {
         System.out.println("Please enter the year you were born:");
         int year = scanner.nextInt();
         System.out.println("The day you were born on is a: " + find.findDay(month,day,year));
+
         //Currency Formatter
         CurrencyFormatter formatting = new CurrencyFormatter();
         System.out.println("Please enter an amount you want to format:");
         double payment = scanner.nextDouble();
         formatting.Converter(payment);
+
         //Username Validator using REGEX
         UsernameValidator valid = new UsernameValidator();
         valid.validate(1,"SaajidParker");
+
         //Creating an array and calling the values added to it
         FirstArrayExercise firstarray = new FirstArrayExercise();
         firstarray.arr();
+
         //Practicing Hashsets
         HashsetPractice pairsCounter = new HashsetPractice();
         System.out.println("Enter the number of unique pairs to add to hashset:");
         int n = scanner.nextInt();
         pairsCounter.processPairs(n, scanner);
+
         //Try and catch practice for dividing by zero
         TryandCatchPractice trying = new TryandCatchPractice();
         trying.Trycatch(scanner);
+
         //Practicing converting a double value to whole and fractional parts
         System.out.println("Please enter a double value:");
         double value = scanner.nextDouble();
         MathProblems mathing = new MathProblems();
         mathing.fractionValue(value);
+
         //Practicing String manipulation and functions
         StringChallengeTwo manipulate = new StringChallengeTwo();
         System.out.println("Let's input and manipulate two string values:");
