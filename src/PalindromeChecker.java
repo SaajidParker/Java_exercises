256518b4f8b5ed87017a13a4eb52d43a7125d699
Saajid Parker <parkersaajid2@gmail.com>
2024-01-11 11:51:34 +0200

    Created a palindrome checker

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+public class PalindromeChecker {
+    public void palindrome(String word){
+        StringBuilder building = new StringBuilder();
+        building.append(word);
+        building.reverse();
+        String reversed = String.valueOf(building);
+        if(reversed.equals(word)){
+            System.out.println("Yes, the word: "+word+" is a palindrome");
+        }else{
+            System.out.println("No, the word: "+word+" is not a palindrome");
+        }
+    }
+}
