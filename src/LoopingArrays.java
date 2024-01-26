8ef3999301c567d4cfebf5530a7a48176f2b7112
Saajid Parker <parkersaajid2@gmail.com>
2024-01-26 15:45:20 +0200

    more changes

--- src/LoopingArrays.java
+++ src/LoopingArrays.java
@@ -7,7 +7,7 @@ public class LoopingArrays {
             sum += ages[x];
         }
         System.out.println(sum);
-
+        sum = 0;
         //Or you can make it a shortcode using for each
         for(int x:ages){
             sum+=x;
