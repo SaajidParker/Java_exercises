1f7c8ecc65d332123a916f2235f30f5d3471f873
Saajid Parker <parkersaajid2@gmail.com>
2024-01-26 16:08:13 +0200

    more changes

--- src/Multidimensional_Array.java
+++ src/Multidimensional_Array.java
@@ -1,11 +1,11 @@
 public class Multidimensional_Array {
-    public void superiorArray(){
+    public void superiorArray(int [][] summing){
         int sum = 0;
         //This is a multidimensional array
         int[][] sample = {{1,2,3},{4,5,6}};
         // To access an element inside a multidimensional array we can use this
         int p = sample[1][0];
-        System.out.println(p);
+
 
         // We can visualize the rows and columns like this where the first value is the row and second value is the column
         sample = new int[][]{
@@ -13,11 +13,11 @@ public class Multidimensional_Array {
                 {4, 5, 6}};
 
         // In order to loop over a multidimensional array we need nested for loops
-        for(int x = 0; x<sample.length;x++){
-            for(int y = 0; y<sample[x].length;y++){
-                sum += sample[x][y];
+        for(int x = 0; x<summing.length;x++){
+            for(int y = 0; y<summing[x].length;y++){
+                sum += summing[x][y];
             }
         }
-        System.out.println(sum);
+        System.out.println("The sum of the multidimensional array used is: "+sum);
     }
 }
