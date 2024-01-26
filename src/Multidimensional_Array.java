5a4a8270fac1733255277bfb64af0513bf915687
Saajid Parker <parkersaajid2@gmail.com>
2024-01-26 16:01:10 +0200

    more changes

--- src/Multidimensional_Array.java
+++ src/Multidimensional_Array.java
@@ -3,8 +3,8 @@ public class Multidimensional_Array {
         //This is a multidimensional array
         int[][] sample = {{1,2,3},{4,5,6}};
         // To access an element inside a multidimensional array we can use this
-        int x = sample[1][0];
-        System.out.println(x);
+        int p = sample[1][0];
+        System.out.println(p);
 
         // We can visualize the rows and columns like this where the first value is the row and second value is the column
         sample = new int[][]{
@@ -12,6 +12,10 @@ public class Multidimensional_Array {
                 {4, 5, 6}};
 
         // In order to loop over a multidimensional array we need nested for loops
-
+        for(int x = 0; x<sample.length;x++){
+            for(int y = 0; y<sample[x].length;y++){
+                System.out.println(sample[x][y]);
+            }
+        }
     }
 }
