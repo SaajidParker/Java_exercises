1821ee364c3fe205eac3791138f10987bd0b9764
Saajid Parker <parkersaajid2@gmail.com>
2024-01-26 15:53:58 +0200

    more changes

--- src/Multidimensional_Array.java
+++ src/Multidimensional_Array.java
@@ -2,6 +2,9 @@ public class Multidimensional_Array {
     public void superiorArray(){
         //This is a multidimensional array
         int[][] sample = {{1,2,3},{4,5,6}};
+        // To access an element inside a multidimensional array we can use this
+        int x = sample[1][0];
+        System.out.println(x);
 
     }
 }
