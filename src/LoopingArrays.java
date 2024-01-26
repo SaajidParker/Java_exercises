05d4cbc5b788c94a0a910ab2f1ca44f85569dbc6
Saajid Parker <parkersaajid2@gmail.com>
2024-01-26 15:40:01 +0200

    Made changes

--- src/LoopingArrays.java
+++ src/LoopingArrays.java
@@ -1,6 +1,11 @@
 public class LoopingArrays {
 
-    public void arraylooping(){
-
+    public void arraylooping(int[] ages){
+        //using a for loop to loop through the arrays using the length function.
+        int sum = 0;
+        for(int x = 0; x<ages.length;x++){
+            sum += ages[x];
+        }
+        System.out.println(sum);
     }
 }
