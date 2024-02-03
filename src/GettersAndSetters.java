7961d6da50cc4d83d151d3e764aafe7d9db5d9cd
Saajid Parker <parkersaajid2@gmail.com>
2024-02-03 22:30:26 +0200

    more changes

--- src/GettersAndSetters.java
+++ src/GettersAndSetters.java
@@ -9,14 +9,5 @@ public class GettersAndSetters {
         //setter method
         this.age = age;
     }
-    public void message(String name, int age){
-        if(age<0){
-            System.out.println("Invalid age");
-            gettersAndSetters.setAge(0);
-        }else{
-            gettersAndSetters.setAge(age);
-        }
-        System.out.println("Name of person is: "+name);
-        System.out.println("Age of person is: "+gettersAndSetters.getAge());
-    }
+
 }
