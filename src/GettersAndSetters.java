b376beb5b535f4593ac29f5027e8f2c68bc7cb9b
Saajid Parker <parkersaajid2@gmail.com>
2024-02-03 22:19:11 +0200

    more changes

--- src/GettersAndSetters.java
+++ src/GettersAndSetters.java
@@ -1,4 +1,11 @@
 public class GettersAndSetters {
     public String name;
+    private int age;
 
+    public int getAge() {
+        return  age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
