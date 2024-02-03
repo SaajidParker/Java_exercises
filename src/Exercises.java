37b4e557138b84123a59238cba4864046daa163c
Saajid Parker <parkersaajid2@gmail.com>
2024-02-03 22:27:54 +0200

    more changes

--- src/Exercises.java
+++ src/Exercises.java
@@ -209,6 +209,7 @@ public class Exercises {
         System.out.println("Please enter the power number:");
         int power = scanner.nextInt();
         mathingClasses.powerOfValue(x,power);
+
         //Using getters and setters
         System.out.println("Please enter an age to see if it meets requirements:");
         int age = scanner.nextInt();
