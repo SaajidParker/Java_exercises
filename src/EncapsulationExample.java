d9f9362c47e2714991df7373a7705232c6b1288f
Saajid Parker <parkersaajid2@gmail.com>
2024-02-08 22:00:03 +0200

    more changes

--- src/EncapsulationExample.java
+++ src/EncapsulationExample.java
@@ -1,3 +1,16 @@
 public class EncapsulationExample {
+    private int age;
 
+    public void setAge(int age) {
+        this.age = age;
+        if(age<6){
+            System.out.println("You are too young");
+        }else{
+            System.out.println("Welcome to Hogwarts!");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
