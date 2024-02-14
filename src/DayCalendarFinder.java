5bcf1d256e22926956a178613669928925e5d28f
Saajid Parker <42733838+SaajidParker@users.noreply.github.com>
2024-02-14 20:31:24 +0200

    Update DayCalendarFinder.java

--- src/DayCalendarFinder.java
+++ src/DayCalendarFinder.java
@@ -2,7 +2,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 public class DayCalendarFinder {
     public  String findDay(int month, int day, int year) {
-
+        //this is a calendar finder 
         LocalDate date = LocalDate.of(year, month, day);
 
         DayOfWeek dayOfWeek = date.getDayOfWeek();
