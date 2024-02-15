03182569582e207a7e0a301621966da6f97b18d5
Saajid Parker <parkersaajid2@gmail.com>
2024-02-15 21:04:17 +0200

    Learning OverLoading and Overriding

---
+++ src/Overloading.java
@@ -0,0 +1,14 @@
+public class Overloading {
+    /*When methods have the same name, but different parameters, it is known as method overloading.
+    This can be very useful when you need the same method functionality for different types of parameters.
+    Another name for method overloading is compile-time polymorphism.*/
+
+    //complete the method for integer value type
+    public static int doubleTheValue(int a) {
+        return a*2;
+    }
+    //overload the method for double value type
+    public static double doubleTheValue(double b) {
+        return b*2;
+    }
+}
